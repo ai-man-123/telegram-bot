2c3855df6c665fe62388a1cc3943717f814e8aa8
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-06-19 11:18:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new TelegramBot(token, { polling: true })
 bot.onText(/\/start$/, (msg) => {
   const chatId = msg.chat.id
   const chatUser = msg.chat.first_name
-  const message = `Добро пожаловать, ${chatUser}!\n\nВы можете скинуть мне ссылку на TikTok откуда нужно выгрузить видео — через пару секунд этот видос будет у вас!\n\nНа данный момент, я поддерживаю только видео из TikTok!`
+  const message = `selamat datang, yang kuban ii aja ye:v, ${chatUser}!\n\nAnda boleh menghantar saya pautan ke TikTok dari tempat anda ingin memuat naik video - dalam beberapa saat anda akan mendapat video ini!\n\nPada masa ini, saya hanya menyokong video daripada TikTok!(ᴀᴡɪꜱʜ``
   bot.sendMessage(chatId, message)
 })
 
@@ -20,7 +20,7 @@ bot.on('message', (msg) => {
     if (message.match('https:\/\/[a-zA-Z]+.tiktok.com\/')) {
       downloadVideo(message).then((videoLink) => bot.sendVideo(chatId, videoLink, { caption: 'Рад был помочь! Ваш, @GetTTVideoBot' }))
     } else {
-      bot.sendMessage(chatId, '⛔️ Вы прислали ссылку, которая не поддерживается ботом!')
+      bot.sendMessage(chatId, '⛔️ Anda menghantar pautan yang tidak disokong atau yang salah oleh bot!')
     }
   }
 })
